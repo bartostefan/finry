c8d6109928cff350f66efde21f05b91fcb698d78
Stefan Bartos <bartos@xostudio.cz>
2017-10-19 20:21:20 +0200

    Small refactoring identify method

--- app/services/fio/identify_item.rb
+++ app/services/fio/identify_item.rb
@@ -13,28 +13,28 @@ module Fio
     private
 
     def identify
-      if @fio_item_id.present?
-        item = Item.new
+      item = Item.new
+      return item unless @fio_item_id.present?
 
-        if fio_item.comment
-          splitted = fio_item.comment.split(':').map(&:strip)
-          item.title = splitted[1]
-          item.description = splitted[0]
-        end
-        item.price = fio_item.amount.abs
-        item.buy_at = fio_item.date
-        item
-      else
-        Item.new
+      if splitted = split_comment(fio_item.comment)
+        item.title, item.description = splitted[0], splitted[1]
       end
+      item.price = fio_item.amount.abs
+      item.buy_at = fio_item.date
+      item
     end
 
     def fio_item
-      @fio_item ||= FioItem.find(@fio_item_id) unless @empty_object
+      @fio_item ||= FioItem.find(@fio_item_id)
     end
 
     def change_to_date(date)
       Date.strptime(date)
+    end
+
+    def split_comment(comment)
+      return nil unless comment
+      comment.split(':').map(&:strip)
     end
   end
 end
