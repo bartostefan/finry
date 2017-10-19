85a15fbada9efefd7b11be032b2f505719acdeee
Stefan Bartos <bartos@xostudio.cz>
2017-10-19 21:41:17 +0200

    Change group function to pgsql

--- app/queries/items/graph_data_query.rb
+++ app/queries/items/graph_data_query.rb
@@ -1,6 +1,6 @@
 module Items
   class GraphDataQuery
-    def self.call(relation = Item.all, options = { includes: :category, group: ['categories.title', 'strftime("%m-%Y", buy_at)'], sum: :price })
+    def self.call(relation = Item.all, options = { includes: :category, group: ['categories.title', "to_char(buy_at, 'MM-YYYY')"], sum: :price })
       relation
         .includes(options[:includes])
         .where.not(buy_at: nil)
