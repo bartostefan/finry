18f282ee9ce7da9e5184accae71ac92a18f3b534
Stefan Bartos <bartos@xostudio.cz>
2017-10-19 21:54:28 +0200

    Add seeds for Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.find_or_create_by!(title: 'Nákupy')
+Category.find_or_create_by!(title: 'Dům')
+Category.find_or_create_by!(title: 'Mlsání')
+Category.find_or_create_by!(title: 'Dárky')
+Category.find_or_create_by!(title: 'Zdraví')
+Category.find_or_create_by!(title: 'Pišta osobní výdaje')
